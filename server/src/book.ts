9c870a987b908ea15e654e6edf9c005de7d06670
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-02-10 15:20:32 +0200

    Update book.ts

--- server/src/book.ts
+++ server/src/book.ts
@@ -1,18 +1,18 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-// 定义接口
+
 interface IBook extends Document {
     name: string;
     author: string;
     pages: number;
 }
 
-// 定义 Schema
+
 const bookSchema: Schema = new Schema({
     name: { type: String, required: true, unique: true },
     author: { type: String, required: true },
     pages: { type: Number, required: true }
 });
 
-// 创建并导出模型
+
 export default mongoose.model<IBook>('Book', bookSchema);
